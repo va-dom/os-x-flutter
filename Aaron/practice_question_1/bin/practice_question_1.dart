30349fc40a8960c52dbe1f2464662c118963e778
va-aaron <aaron.claudio@via-appia.ph>
2024-02-07 09:49:34 +0800

    Initial commit

---
+++ Aaron/practice_question_1/bin/practice_question_1.dart
@@ -0,0 +1,5 @@
+import 'package:practice_question_1/practice_question_1.dart' as practice_question_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practice_question_1.calculate()}!');
+}
