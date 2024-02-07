bd0deeaac532229845cdac2f528f000a4a27dabe
va-aaron <aaron.claudio@via-appia.ph>
2024-02-07 11:11:07 +0800

    test commit

--- Aaron/practice_question_1/bin/practice_question_1.dart
+++ Aaron/practice_question_1/bin/practice_question_1.dart
@@ -1,5 +1,25 @@
 import 'package:practice_question_1/practice_question_1.dart' as practice_question_1;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${practice_question_1.calculate()}!');
+  print('-------------------Practice Question 1-------------------');
+  print('1. Write a program to print your name in Dart.');
+  print('${practice_question_1.myName()}');
+  print("2.a. Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.");
+  print('${practice_question_1.hello1()}');
+  print("2.b. Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.");
+  print('${practice_question_1.hello2()}');
+  print('3. Declare constant type of int set value 7.');
+  print('${practice_question_1.constNum()}');
+  print('4. Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100');
+  print('${practice_question_1.getInterest()}');
+  print('5. Write a program to print a square of a number using user input.');
+  print("Enter number:");
+  int? number = int.parse(stdin.readLineSync()!);
+  for(int i = 0; i <= number; i++){
+    for(int j = 0; j <= number; j++){
+      stdout.write('$j ');
+    }
+    print('');
+  }
 }
--- Aaron/practice_question_1/lib/practice_question_1.dart
+++ Aaron/practice_question_1/lib/practice_question_1.dart
@@ -1,3 +1,28 @@
-int calculate() {
-  return 6 * 7;
+//Question 1
+String myName(){
+  return "Aaron Claudio";
+}
+
+//Question 2
+String hello1(){
+  return 'Hello I am ${myName()}';
+}
+
+String hello2(){
+  return "Hello I'm ${myName()}";
+}
+
+//Question 3
+int constNum(){
+  const num = 7;
+  return num;
+}
+
+//Question 4
+double getInterest(){
+  int p = 150;
+  int t = 75;
+  int r = 23;
+  double interest = (p * t * r) / 100;
+  return interest;
 }
