b40e371f14d81b5f86d6ca0664fcc03280735b66
va-aaron <aaron.claudio@via-appia.ph>
2024-02-07 16:04:40 +0800

    Removed warning string interpolation

--- Aaron/practice_question_1/bin/practice_question_1.dart
+++ Aaron/practice_question_1/bin/practice_question_1.dart
@@ -5,21 +5,21 @@ void main(List<String> arguments) {
   print('-------------------Practice Question 1-------------------');
   //Question 1
   print('1. Write a program to print your name in Dart.');
-  print('${practice_question_1.myName()}');
+  print(practice_question_1.myName());
 
   //Question 2
   print("2.a. Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.");
-  print('${practice_question_1.hello1()}');
+  print(practice_question_1.hello1());
   print("2.b. Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.");
-  print('${practice_question_1.hello2()}');
+  print(practice_question_1.hello2());
 
   //Question 3
   print('3. Declare constant type of int set value 7.');
-  print('${practice_question_1.constNum()}');
+  print(practice_question_1.constNum());
 
   //Question 4
   print('4. Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100');
-  print('${practice_question_1.getInterest()}');
+  print(practice_question_1.getInterest());
 
   //Question 5
   print('5. Write a program to print a square of a number using user input.');
