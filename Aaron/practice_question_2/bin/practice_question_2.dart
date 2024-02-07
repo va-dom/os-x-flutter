ca106f274f786a46da92eb873e3e901b0d1a5036
va-aaron <aaron.claudio@via-appia.ph>
2024-02-07 14:49:25 +0800

    Initial commit for Practice Question 2

---
+++ Aaron/practice_question_2/bin/practice_question_2.dart
@@ -0,0 +1,5 @@
+import 'package:practice_question_2/practice_question_2.dart' as practice_question_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practice_question_2.calculate()}!');
+}
