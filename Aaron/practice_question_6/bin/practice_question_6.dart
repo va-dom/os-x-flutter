20c5057724717af07a659949d73e4f581ef7185d
va-aaron <aaron.claudio@via-appia.ph>
2024-02-08 15:54:14 +0800

    Initial commit for Practice Question 6

---
+++ Aaron/practice_question_6/bin/practice_question_6.dart
@@ -0,0 +1,5 @@
+import 'package:practice_question_6/practice_question_6.dart' as practice_question_6;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practice_question_6.calculate()}!');
+}
---
+++ Aaron/practice_question_6/test/practice_question_6_test.dart
@@ -0,0 +1,8 @@
+// import 'package:practice_question_6/practice_question_6.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
