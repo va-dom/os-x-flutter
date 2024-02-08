04e2d3fafd43872080ce516575f4cdbbd2c43aea
va-aaron <aaron.claudio@via-appia.ph>
2024-02-08 14:47:02 +0800

    Completed Practice Question 4

--- Aaron/practice_question_4/test/practice_question_4_test.dart
+++ Aaron/practice_question_4/test/practice_question_4_test.dart
@@ -1,8 +1,8 @@
-import 'package:practice_question_4/practice_question_4.dart';
-import 'package:test/test.dart';
+// import 'package:practice_question_4/practice_question_4.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
