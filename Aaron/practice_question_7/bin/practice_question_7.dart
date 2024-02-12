ce45e05ac34faa783e58b842416d741375abb6ed
va-aaron <aaron.claudio@via-appia.ph>
2024-02-12 15:25:41 +0800

    Completed Practice Question 7

---
+++ Aaron/practice_question_7/bin/practice_question_7.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+
+import 'package:practice_question_7/address_class.dart';
+
+void main(List<String> arguments) {
+
+  print("-------------------Practice Question 7-------------------");
+  
+  // //Question 2
+  // print("2. Create a late variable named address, assign a US value to it and print it.");
+  // Address address = Address();
+  // address.setName = "Aaron";
+  // print(address.getName);
+
+  // //Question 4
+  // print("4. Write a program in a dart to create an age variable and assign a null value to it using ?.");
+  // int? age;
+  // print(age);
+
+  // //Question 5
+  // print("5. Write a function that accepts a nullable int parameter and returns 0 if the value is null using null coalescing operator ??.");
+  // int nullCheck(int? num) => num ?? 0;
+
+  // int? num1 = 10;
+  // int? num2;
+
+  // print(nullCheck(num1));
+  // print(nullCheck(num2));
+
+  // //Question 6
+  // print("6. Write a function named generateRandom() in dart that randomly returns 100 or null. Also, assign a return value of the function to a variable named status that can’t be null. Give status a default value of 0, if generateRandom() function returns null.");
+  // int? generateRnadom(){
+  //   return Random().nextBool() ? 100 : null;
+  // }
+  // int status = generateRnadom() ?? 0;
+  // print(status);
+
+}
