a05d41fb76c69a31a5e46ed7b59b06536fd423a4
Robertson Morales <moralesso152842@gmail.com>
2024-02-20 13:58:39 +0800

    initial commit of trebs

---
+++ bin/dart_practices.dart
@@ -0,0 +1,5 @@
+import 'package:dart_practices/dart_practices.dart' as dart_practices;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_practices.calculate()}!');
+}
---
+++ test/dart_practices_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_practices/dart_practices.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
