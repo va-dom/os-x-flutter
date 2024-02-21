848bb2922b46bfac25ab5195c0116a606f2b1722
Robertson Morales <robertson.morales@via-appia.ph>
2024-02-21 16:29:07 +0800

    added: practice #3

--- bin/dart_practices.dart
+++ bin/dart_practices.dart
@@ -1,26 +1,18 @@
-import 'practice_1/index.dart' as p1;
-import 'practice_2/index.dart' as p2;
+// import 'practice_1/index.dart' as p1;
+// import 'practice_2/index.dart' as p2;
+import 'practice_3/index.dart' as p3;
 
 void main(List<String> arguments) {
-  // p1.printName();
-  // p1.introduce();
-  // p1.constType();
-  // p1.simpleInterest();
-  // p1.squareOfANumber();
-  // p1.displayFullName();
-  // p1.findQuotientAndRemainder(55, 22);
-  // p1.swapTwoNumbers();
-  // p1.removeWhitespacesFromString();
-  // p1.splitBill();
-  // p1.calculateTimeToOffice();
-
-  // p2.isEvenOrOdd();
-  // p2.checkVowelOrConsonant();
-  // p2.checkNumber();
-  // p2.printName100x();
-  // p2.calculateSumOfNaturalNumbers();
-  // p2.multiplicationOf5();
-  // p2.multiplicationOf1To9();
-  // p2.simpleCalculator();
-  p2.print1To100ButNot41();
+  // p3.callPrintNameFromPractice1();
+  // p3.printEvenNumbers();
+  // p3.greet("Robertson");
+  // p3.generateRandomPassword();
+  // p3.findAreaOfCircle(50.0);
+  // p3.reverseString("Racecar");
+  // p3.calculatePower(5, 3);
+  // p3.add(5, 3);
+  // p3.maxNumber(5, 3, 11);
+  // p3.isEven(4);
+  // p3.createUser("Robertson Morales", 24, false);
+  // p3.calculateArea(3);
 }
---
+++ bin/practice_3/index.dart
@@ -0,0 +1,80 @@
+import '../practice_1/index.dart' as p1;
+import 'dart:math';
+
+// Write a program in Dart to print your own name using function.
+void callPrintNameFromPractice1() {
+  p1.printName();
+}
+
+// Write a program in Dart to print even numbers between intervals using function.
+void printEvenNumbers() {
+  const count = 100;
+
+  for (int i = 1; i <= count; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+}
+
+// Create a function called greet that takes a name as an argument and prints a greeting message. For example, greet(“John”) should print “Hello, John”.
+void greet(String name) {
+  print("Hello, $name");
+}
+
+// Write a program in Dart that generates random password.
+void generateRandomPassword() {
+  const passwordLength = 8, intRange = 10;
+  Random random = Random();
+  List<int> password =
+      List.generate(passwordLength, (_) => random.nextInt(intRange));
+
+  print(password.join(''));
+}
+
+// Write a program in Dart that find the area of a circle using function. Formula: pi * r * r
+void findAreaOfCircle(double radius) {
+  double area = pi * radius * radius;
+  print(area);
+}
+
+// Write a program in Dart to reverse a String using function.
+void reverseString(String str) {
+  print(str.split('').reversed.join(''));
+}
+
+// Write a program in Dart to calculate power of a certain number. For e.g 5^3=125
+void calculatePower(int base, int exponent) {
+  print(pow(base, exponent));
+}
+
+// Write a function in Dart named add that takes two numbers as arguments and returns their sum.
+void add(int num1, int num2) {
+  print(num1 + num2);
+}
+
+// Write a function in Dart called maxNumber that takes three numbers as arguments and returns the largest number.
+void maxNumber(int num1, int num2, int num3) {
+  print(max(num1, max(num2, num3)));
+}
+
+// Write a function in Dart called isEven that takes a number as an argument and returns True if the number is even, and False otherwise.
+void isEven(int num) {
+  if (num % 2 == 0) {
+    print(true);
+  } else {
+    print(false);
+  }
+}
+
+// Write a function in Dart called createUser with parameters name, age, and isActive, where isActive has a default value of true.
+void createUser(String name, int age, [bool isActive = true]) {
+  print("Name: $name");
+  print("Age: $age");
+  print("isActive: $isActive");
+}
+
+// Write a function in Dart called calculateArea that calculates the area of a rectangle. It should take length and width as arguments, with a default value of 1 for both. Formula: length * width.
+void calculateArea([int length = 1, int width = 1]) {
+  print(length * width);
+}
