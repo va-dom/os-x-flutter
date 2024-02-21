42a0f6e7274f28ccbc8a82e0487690e0e5d18024
va-aaron <aaron.claudio@via-appia.ph>
2024-02-21 17:05:05 +0800

    deleted practice_1.dart

--- Dart/aaron_claudio/practice_1.dart/bin/practice_1.dart
+++
@@ -1,5 +0,0 @@
-import 'package:practice_1/practice_1.dart' as practice_1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${practice_1.calculate()}!');
-}
--- Dart/aaron_claudio/practice_1.dart/test/practice_1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:practice_1/practice_1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
