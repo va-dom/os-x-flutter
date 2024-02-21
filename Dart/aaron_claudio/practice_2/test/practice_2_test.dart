6cc38d51aad3f17156059bc2f03c2b9d2c66c2d6
va-aaron <aaron.claudio@via-appia.ph>
2024-02-21 15:42:35 +0800

    transfer practice_2 to Dart folder with minor changes

---
+++ Dart/aaron_claudio/practice_2/test/practice_2_test.dart
@@ -0,0 +1,8 @@
+// import 'package:practice_2/practice_2.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
