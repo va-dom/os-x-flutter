c16dbe74d4ce39bf049ef0e623bda7eab1750861
Robertson Morales <robertson.morales@via-appia.ph>
2024-02-22 18:34:12 +0800

    added: practice #7

--- bin/dart_practices.dart
+++ bin/dart_practices.dart
@@ -1,15 +1,11 @@
 // import 'practice_1/index.dart' as p1;
 // import 'practice_2/index.dart' as p2;
 // import 'practice_3/index.dart' as p3;
-import 'practice_4/index.dart' as p4;
+// import 'practice_4/index.dart' as p4;
+import 'practice_7/index.dart' as p7;
 
 void main(List<String> arguments) {
-  // p4.printAllNames();
-  // p4.printAllFruit();
-  // p4.printTotalExpenses();
-  // p4.printDays();
-  // p4.printFriendsStartingWithA();
-  // p4.updateCountry();
-  // p4.findKeysWithLength4();
-  p4.createToDoApp();
+  // p7.printNullableType();
+  // p7.nullCoalescingOperator(null);
+  p7.triggerGenerateRandom();
 }
---
+++ bin/practice_7/index.dart
@@ -0,0 +1,38 @@
+// Create a late variable named address, assign a US value to it and print it.
+import 'dart:math';
+
+void printAddress() {
+  late String address;
+  address = 'US';
+  print(address);
+}
+
+// How do you declare a nullable type in Dart null safety?
+void printNullableType() {
+  String? address;
+
+  print(address);
+}
+
+// Write a program in a dart to create an age variable and assign a null value to it using ?.
+void printAge() {
+  int? age;
+  print(age);
+}
+
+// Write a function that accepts a nullable int parameter and returns 0 if the value is null using null coalescing operator ??.
+void nullCoalescingOperator(int? num) {
+  int result = num ?? 0;
+  print(result);
+}
+
+int? generateRandom() {
+  return Random().nextBool() ? 100 : null;
+}
+
+// Write a function named generateRandom() in dart that randomly returns 100 or null. Also, assign a return value of the function to a variable named status that can’t be null. Give status a default value of 0, if generateRandom() function returns null.
+void triggerGenerateRandom() {
+  int status = generateRandom() ?? 0;
+
+  print(status);
+}
