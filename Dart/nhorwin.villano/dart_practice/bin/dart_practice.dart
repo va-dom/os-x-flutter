02b2b3c46ed994ad2f59c7a4f40d863a8c44c6d1
Nhorwin Villano <nhorwin.villano@via-appia.ph>
2024-02-22 22:24:48 +0800

    Update Practice 1

--- Dart/nhorwin.villano/dart_practice/bin/dart_practice.dart
+++ Dart/nhorwin.villano/dart_practice/bin/dart_practice.dart
@@ -1,5 +1,6 @@
 import 'package:dart_practice/dart_practice.dart' as dart_practice;
+import 'practice1.dart' as practice1;
 
-void main(List<String> arguments) {
+void main() {
   print('Hello world: ${dart_practice.calculate()}!');
 }
