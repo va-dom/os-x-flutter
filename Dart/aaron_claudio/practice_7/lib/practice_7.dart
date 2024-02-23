f4d1cf871e2abfbc21dcc1f24c72e247ea39b4c3
va-aaron <aaron.claudio@via-appia.ph>
2024-02-23 09:23:16 +0800

    transfer practice_7 to Dart folder with minor changes

---
+++ Dart/aaron_claudio/practice_7/lib/practice_7.dart
@@ -0,0 +1,7 @@
+class Address{
+  late String name;
+
+  set setName(String name) => this.name = name;
+  String get getName => name;
+
+}
---
+++ Dart/aaron_claudio/practice_7/test/practice_7_test.dart
@@ -0,0 +1,8 @@
+// import 'package:practice_7/practice_7.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
