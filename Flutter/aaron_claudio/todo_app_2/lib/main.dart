2837b49dda526dce812e2bc11b83d45a89af73ce
va-aaron <aaron.claudio@via-appia.ph>
2024-02-28 16:59:35 +0800

    set up firebase

--- Flutter/aaron_claudio/todo_app_2/lib/main.dart
+++ Flutter/aaron_claudio/todo_app_2/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app_2/firebase_options.dart';
 import 'package:todo_app_2/screens/task_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
