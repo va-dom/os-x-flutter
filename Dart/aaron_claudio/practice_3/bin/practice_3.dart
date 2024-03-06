4ef03dcea08e0bc0951427da178f7e5a56665919
va-aaron <aaron.claudio@via-appia.ph>
2024-03-06 14:01:16 +0800

    minor changes on practice_3

--- Dart/aaron_claudio/practice_3/bin/practice_3.dart
+++ Dart/aaron_claudio/practice_3/bin/practice_3.dart
@@ -14,9 +14,9 @@ void main(List<String> arguments) {
   //Question 2
   print('2. Write a program in Dart to print even numbers between intervals using function.');
   print('Enter first number:');
-  int? num1 = int.parse(stdin.readLineSync()!);
+  int num1 = int.parse(stdin.readLineSync()!);
   print('Enter second number:');
-  int? num2 = int.parse(stdin.readLineSync()!);
+  int num2 = int.parse(stdin.readLineSync()!);
   print(practice_3.getEvenNumbers(num1, num2));
 
   print("\n---------------------------------------------------------------------------------------------------------------\n");
@@ -36,7 +36,7 @@ void main(List<String> arguments) {
   //Question 5
   print('5. Write a program in Dart that find the area of a circle using function. Formula: pi * r * r');
   print('Enter the radius of the circle:');
-  double? r = double.parse(stdin.readLineSync()!);
+  double r = double.parse(stdin.readLineSync()!);
   print('The area of a circle with a radius of $r is: ${practice_3.getArea(r)}');
 
   print("\n---------------------------------------------------------------------------------------------------------------\n");
@@ -52,9 +52,9 @@ void main(List<String> arguments) {
   //Question 7
   print('7. Write a program in Dart to calculate power of a certain number. For e.g 5^3=125');
   print('Enter base number:');
-  num? base = num.parse(stdin.readLineSync()!);
+  num base = num.parse(stdin.readLineSync()!);
   print('Enter exponent:');
-  num? expnt = num.parse(stdin.readLineSync()!);
+  num expnt = num.parse(stdin.readLineSync()!);
   print('$base ^ $expnt = ${practice_3.calculatePow(base, expnt)}');
 
   print("\n---------------------------------------------------------------------------------------------------------------\n");
@@ -62,9 +62,9 @@ void main(List<String> arguments) {
   //Question 8
   print('8. Write a function in Dart named add that takes two numbers as arguments and returns their sum.');
   print('Enter base number:');
-  int? num4 = int.parse(stdin.readLineSync()!);
+  int num4 = int.parse(stdin.readLineSync()!);
   print('Enter exponent:');
-  int? num5 = int.parse(stdin.readLineSync()!);
+  int num5 = int.parse(stdin.readLineSync()!);
   print('The sum of $num4 and $num5 is: ${practice_3.add(num4, num5)}');
 
   print("\n---------------------------------------------------------------------------------------------------------------\n");
@@ -72,11 +72,11 @@ void main(List<String> arguments) {
   //Question 9
   print('9. Write a function in Dart called maxNumber that takes three numbers as arguments and returns the largest number.');
   print('Enter first number:');
-  int? num6 = int.parse(stdin.readLineSync()!);
+  int num6 = int.parse(stdin.readLineSync()!);
   print('Enter second number:');
-  int? num7 = int.parse(stdin.readLineSync()!);
+  int num7 = int.parse(stdin.readLineSync()!);
   print('Enter third number:');
-  int? num8 = int.parse(stdin.readLineSync()!);
+  int num8 = int.parse(stdin.readLineSync()!);
   print('The largest number between $num6, $num7, and $num8 is: ${practice_3.maxNumber(num6, num7, num8)}');
 
   print("\n---------------------------------------------------------------------------------------------------------------\n");
@@ -84,7 +84,7 @@ void main(List<String> arguments) {
   //Question 10
   print('10. Write a function in Dart called isEven that takes a number as an argument and returns True if the number is even, and False otherwise.');
   print('Enter number:');
-  int? num9 = int.parse(stdin.readLineSync()!);
+  int num9 = int.parse(stdin.readLineSync()!);
   print('Is $num9 an even number? ${practice_3.isEven(num9)}');
 
   print("\n---------------------------------------------------------------------------------------------------------------\n");
@@ -92,9 +92,9 @@ void main(List<String> arguments) {
   //Question 11
   print('11. Write a function in Dart called createUser with parameters name, age, and isActive, where isActive has a default value of true.');
   print('Enter your Name');
-  String? name = stdin.readLineSync()!;
+  String name = stdin.readLineSync()!;
   print('Enter your age');
-  int? age = int.parse(stdin.readLineSync()!);
+  int age = int.parse(stdin.readLineSync()!);
   Map<String, dynamic> user1 = practice_3.createUser(name, age);
   print('User successfully created.');
   print('Name: ${user1['name']}');
@@ -106,8 +106,8 @@ void main(List<String> arguments) {
   //Question 12
   print('12. Write a function in Dart called calculateArea that calculates the area of a rectangle. It should take length and width as arguments, with a default value of 1 for both. Formula: length * width.');
   print('Enter length:');
-  double? length = double.parse(stdin.readLineSync()!);
+  double length = double.parse(stdin.readLineSync()!);
   print('Enter width:');
-  double? width = double.parse(stdin.readLineSync()!);
+  double width = double.parse(stdin.readLineSync()!);
   print('The area of a reactangle with length of $length and width of $width is: ${practice_3.calculateArea(length, width)}');
 }
